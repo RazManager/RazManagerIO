7c5a69a8019a2396dd005a737ee16aaccbbeec99
Anders Ryberg <anders@razlogic.se>
2023-11-09 07:24:08 +0100

    BluezApplication WatchInterfacesAddedAsync

--- dotnet/RazManagerIO/RazManagerIO.Host/Services/Utilities/BluezGattApplication.cs
+++ dotnet/RazManagerIO/RazManagerIO.Host/Services/Utilities/BluezGattApplication.cs
@@ -4,19 +4,23 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tmds.DBus;
 
+
 namespace RazManagerIO.Host.Services.Utilities
 {
     [DBusInterface("org.freedesktop.DBus.ObjectManager")]
     public class BluezGattApplication : IObjectManager
     {
         private readonly ObjectPath _objectPath;
-        private BluezGattService _gattService;
+        private BluezGattService? _gattService;
 
 
         public BluezGattApplication(string objectPath)
         {
             _objectPath = objectPath;
+            InterfacesAdded += InterfacesAddedHandler;
+            InterfacesRemoved += InterfacesRemovedHandler;
         }
+
 
         public ObjectPath ObjectPath => _objectPath;
 
@@ -37,14 +41,40 @@ namespace RazManagerIO.Host.Services.Utilities
             return Task.FromResult(result);
         }
 
+
         public Task<IDisposable> WatchInterfacesAddedAsync(Action<(ObjectPath @object, IDictionary<string, IDictionary<string, object>> interfaces)> handler, Action<Exception> onError = null)
         {
-            throw new NotImplementedException();
+            return SignalWatcher.AddAsync(this, nameof(InterfacesAdded), handler);
         }
+
+        public event Action<(ObjectPath @object, IDictionary<string, IDictionary<string, object>> interfaces)> InterfacesAdded;
+
+        public void InterfacesAddedHandler((ObjectPath @object, IDictionary<string, IDictionary<string, object>> interfaces) e)
+        {
+            //Console.WriteLine($"{args.objectPath} added.");
+            Console.WriteLine($"{this.GetType().Name}: {e.@object} added with the following interfaces...");
+            foreach (var ifacekp in e.interfaces)
+            {
+                Console.WriteLine(ifacekp.Key);
+            }
+        }
+
 
         public Task<IDisposable> WatchInterfacesRemovedAsync(Action<(ObjectPath @object, string[] interfaces)> handler, Action<Exception> onError = null)
         {
-            throw new NotImplementedException();
+            return SignalWatcher.AddAsync(this, nameof(InterfacesRemoved), handler);
+        }
+
+        public event Action<(ObjectPath @object, string[] interfaces)> InterfacesRemoved;
+
+        public void InterfacesRemovedHandler((ObjectPath @object, string[] interfaces) e)
+        {
+            //Console.WriteLine($"{args.objectPath} added.");
+            Console.WriteLine($"{this.GetType().Name}: {e.@object} removed the following interfaces...");
+            foreach (var iface in e.interfaces)
+            {
+                Console.WriteLine(iface);
+            }
         }
 
 
